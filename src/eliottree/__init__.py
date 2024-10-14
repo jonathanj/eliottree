f202af85764268c402b3c3282dc1d5955d6b5510
Sami Liedes <sami.liedes@iki.fi>
2024-10-14 13:07:37 +0200

    Bump versioneer to 0.29

--- src/eliottree/__init__.py
+++ src/eliottree/__init__.py
@@ -16,6 +16,5 @@ __all__ = [
     'colored',
 ]
 
-from ._version import get_versions  # noqa: E402
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
